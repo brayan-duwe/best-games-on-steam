bd7bc49ddfd01f94ceb4cd66d93b46c883aa1466
Brayan Duwe <91811797+brayan-duwe@users.noreply.github.com>
2026-07-11 21:55:26 -0300

    feat: add price formatting, review categorization and column filtering

--- src/process_data.py
+++ src/process_data.py
@@ -1,10 +1,45 @@
 import json
 import pandas as pd
 
+COL_NAME = "name"
+COL_PRICE = "price"
+COL_POSITIVE = "positive"
+COL_NEGATIVE = "negative"
+COL_DEVELOPER = "developer"
+COL_REVIEWS = "reviews"
 
-with open("data/raw_data.json", "r") as content:
-    data = json.load(content)
+def sheet_data(): 
+    with open("data/raw_data.json", "r") as content:
+        data = json.load(content)
+    result = pd.json_normalize(list(data.values()))
 
+    return result
 
-df = pd.json_normalize(list(data.values()))
-print(df)
+def dollar_converter(data):
+    data[COL_PRICE] = (pd.to_numeric(data[COL_PRICE]) / 100).round(2)
+    data[COL_PRICE] = data[COL_PRICE].apply(lambda x: "Free" if x == 0.0 else f"${x:.2f}")
+
+    return data
+
+def average_reviews(data):
+    data[COL_POSITIVE] = data[COL_POSITIVE] / (data[COL_POSITIVE] + data[COL_NEGATIVE]) * 100
+
+    bins = [0, 40, 60, 80, 100]
+    labels = ["Mostly negative", "Neutral", "Mostly positive", "Positive"]
+    data[COL_REVIEWS] = pd.cut(data[COL_POSITIVE], bins=bins, labels=labels)
+
+    return data
+
+def data_filter(data):
+    filtered_data = data[[COL_NAME, COL_DEVELOPER, COL_PRICE, COL_REVIEWS]]
+    filtered_data = filtered_data.rename(columns={COL_NAME: "Name", COL_DEVELOPER: "Developer", COL_PRICE: "Price", COL_REVIEWS: "Reviews"})
+
+    return filtered_data
+
+def get_processed_data():
+    data = sheet_data()
+    data = dollar_converter(data)
+    data = average_reviews(data)
+    filtered_data = data_filter(data)
+
+    return filtered_data
